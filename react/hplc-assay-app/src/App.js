e5b59e757de5634455b9e6b3434848c7c452b3e2
DAIN302 <dbgnfk12@gmail.com>
2024-08-12 15:43:54 +0900

    practice

--- react/hplc-assay-app/src/App.js
+++ react/hplc-assay-app/src/App.js
@@ -7,13 +7,20 @@ function App() {
   const [stdPeak, setStdPeak] = useState([]) // 첫번째 - 평균, 두번째 - 표준편차, 세번째 - RSD 
 
   const stdResult = useMemo(() => {
+    getStdResult()
     
-  })
+  }, [stdPeak])
+
+  const getStdResult = () => {
+      // let stdAvg = reduce((acc,cur, i, {length}) => i === length - 1?(acc+cur) / length : acc + cur, 0)
+      // let stdSD = 
+      // let stdRSD = 
+  }
 
   return (
     <div className="App">
       <StandardPeak />
-      <StandardResult />
+      <StandardResult result={stdPeak}/>
     </div>
   );
 }
