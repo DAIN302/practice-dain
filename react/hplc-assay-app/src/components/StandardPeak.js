29dfb71a95a854b78e5547fee7a75636f1c76185
DAIN302 <dbgnfk12@gmail.com>
2024-08-14 13:29:00 +0900

    practice

--- react/hplc-assay-app/src/components/StandardPeak.js
+++ react/hplc-assay-app/src/components/StandardPeak.js
@@ -1,11 +1,20 @@
 import React, {useReducer, useRef} from 'react'
 
-const initPeak = []
+const initPeak = [0,0,0,0,0,0]
 
 const reducer = (state, action) => {
-  console.log(action);
-  
-  // return state[action - 1] = inputRef.current[action-1].value()
+  let peakIndex = (action.inputIdNum) - 1
+  let peakValue = Number(action.inputValue)
+
+  switch(peakIndex) {
+    case 0 : return state[0] = peakValue;
+    case 1 : return state[1] = peakValue;
+    case 2 : return state[2] = peakValue;
+    case 3 : return state[3] = peakValue;
+    case 4 : return state[4] = peakValue;
+    case 5 : return state[5] = peakValue;
+    default :  return state[peakIndex] = 0;
+  }
 }
 
 export default function StandardPeak() {
@@ -16,10 +25,11 @@ export default function StandardPeak() {
     const inputArr = [1,2,3,4,5,6]
 
     const stdArr = (e) => {
-      let inputId = e.currentTarget.getAttribute('id')
-      dispatch(inputId)
-      console.log(peak);
-      
+      let inputId = e.currentTarget.getAttribute('id');
+      let inputValue = e.currentTarget.value;
+      let inputIdNum = [...inputId].filter(num=>!isNaN(num)).join()
+      inputIdNum = Number(inputIdNum)
+      dispatch({inputIdNum, inputValue})
     }
   return (
     <div>
