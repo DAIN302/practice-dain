b08d0f89a20eec2a03a74152d73ae4c956005644
DAIN302 <dbgnfk12@gmail.com>
2024-08-14 18:59:40 +0900

    practice

--- react/hplc-assay-app/src/components/StandardResult.js
+++ react/hplc-assay-app/src/components/StandardResult.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function StandardResult(props) {
   return (
     <div>
-        <input type='number' id='stdAvg' placeholder='평균' disabled/>
-        <input type='number' id='stdSD' placeholder='표준편차' disabled/>
-        <input type='number' id='stdRSD' placeholder='RSD' disabled/>
+        <input type='number' id='stdAvg' placeholder='평균' readOnly/>
+        <input type='number' id='stdSD' placeholder='표준편차' readOnly/>
+        <input type='number' id='stdRSD' placeholder='RSD' readOnly/>
     </div>
   )
 }
