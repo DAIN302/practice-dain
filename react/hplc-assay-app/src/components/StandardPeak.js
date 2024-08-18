e305c8c13d7ce70de4c88395a6d1d1e10054c485
DAIN302 <dbgnfk12@gmail.com>
2024-08-18 23:02:49 +0900

    pratice
    배열로 전달 완료

--- react/hplc-assay-app/src/components/StandardPeak.js
+++ react/hplc-assay-app/src/components/StandardPeak.js
@@ -1,41 +1,33 @@
-import React, {useReducer, useRef} from 'react'
+import React, {useRef, useState} from 'react'
 
-const initPeak = [0,0,0,0,0,0]
-
-const reducer = (state, action) => {
-  let peakIndex = (action.inputIdNum) - 1
-  let peakValue = Number(action.inputValue)
-
-  switch(peakIndex) {
-    case 0 : return state[0] = peakValue;
-    case 1 : return state[1] = peakValue;
-    case 2 : return state[2] = peakValue;
-    case 3 : return state[3] = peakValue;
-    case 4 : return state[4] = peakValue;
-    case 5 : return state[5] = peakValue;
-    default :  return state[peakIndex] = 0;
-  }
-}
-
-export default function StandardPeak() {
-    const [peak, dispatch] = useReducer(reducer, initPeak)
+export default function StandardPeak({addPeak}) {
+    const [stdPeak, setStdPeak] =  useState([0,0,0,0,0,0])
 
     const inputRef = useRef([])
 
     const inputArr = [1,2,3,4,5,6]
 
-    const stdArr = (e) => {
-      let inputId = e.currentTarget.getAttribute('id');
-      let inputValue = e.currentTarget.value;
-      let inputIdNum = [...inputId].filter(num=>!isNaN(num)).join()
-      inputIdNum = Number(inputIdNum)
-      dispatch({inputIdNum, inputValue})
+    const addPeakRes = (e, idx) => {
+      const res = Number(e.target.value);
+      const peakarr = stdPeak.map((v,i)=>{
+        if(i === idx) return res;
+        else return v;
+      })
+
+      setStdPeak(peakarr) 
+      // 부모에게 전달
+      addPeak(peakarr)
     }
+
+    console.log('자식',stdPeak);
+    
+
   return (
     <div>
       {
         inputArr.map((number) => 
-          <input key={number} type='number' id={'std'+(number)} onBlur={stdArr} ref={el => inputRef.current[number - 1] = el} />
+          <input key={number} type='number' id={'std'+(number)} 
+        ref={el => inputRef.current[number - 1] = el} onBlur={(e)=>addPeakRes(e, number-1)}/>
         )
       }
     </div>
