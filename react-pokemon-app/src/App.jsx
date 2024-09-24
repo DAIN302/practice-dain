5a650b2742ad9152d3dbf549df6c9ba938b9faf0
DAIN302 <dbgnfk12@gmail.com>
2024-09-24 22:47:30 +0900

    포켓몬 도감

--- react-pokemon-app/src/App.jsx
+++ react-pokemon-app/src/App.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DetailPage from "./pages/DetailPage";
-import MainPage from "./pages/MainPage";
+import React from "react"
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import DetailPage from "./pages/DetailPage"
+import MainPage from "./pages/MainPage"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/pokemon/:id" element={<DetailPage />} />
+        <Route path="/" element={<MainPage />}/>
+        <Route path="/pokemon/:id" element={<DetailPage />}/>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- react-pokemon-app/src/pages/DetailPage/index.jsx
+++ react-pokemon-app/src/pages/DetailPage/index.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
-  return (
-    <div>index</div>
-  )
-}
+  const params = useParams();
+  const pokemonId = params.id;
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
 
-export default DetailPage
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
+  // 개별 포켓몬 데이터 가져오기
+  const fetchPokemonData = async () => {
+    const url = `${baseUrl}${pokemonId}`;
+    try {
+      const { data: pokemonData } = await axios.get(url);
+
+      if (pokemonData) {
+        const { name, id, types, weight, height, stats, abilities } =
+          pokemonData;
+
+        // 이전 포켓몬 다음 포켓몬 이름 가져오기
+        const nextAndPrviousPokemon = await getNextAndPreviousPokemon(id);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 이전 포켓몬, 다음 포켓몬 이름 가져오는 함수
+  const getNextAndPreviousPokemon = async (id) => {
+    const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
+    const { data: pokemonData } = await axios.get(urlPokemon);
+
+    const nextResponse =
+      pokemonData.next && (await axios.get(pokemonData.next));
+    const previousResponse =
+      pokemonData.previous && (await axios.get(pokemonData.previous));
+
+    return {
+      next: nextResponse?.data?.results?.[0]?.name,
+      previous: previousResponse?.data?.results?.[0]?.name,
+    };
+  };
+
+  return <div></div>;
+};
+
+export default DetailPage;
