84de3e2cc598d3c6fb7a8bec1a86f447e9f67a29
DAIN302 <dbgnfk12@gmail.com>
2024-10-11 21:56:11 +0900

    여행 상품 주문 페이지

--- react/react-context-app/client/src/pages/SummaryPage/index.js
+++ react/react-context-app/client/src/pages/SummaryPage/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SummaryPage = () => {
-  return (
-    <div>SummaryPage</div>
-  )
-}
+  // 주문 확인 체크를 해야 주문 확인 버튼 누를 수 있음
 
-export default SummaryPage
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div>
+      <form>
+        <input
+          type="checkbox"
+          checked={checked}
+          id="confirm-checkbox"
+          onChange={(e) => setChecked(e.target.checked)}
+        />{" "}
+        <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
+        <br />
+        <button type="submit" disabled={!checked}>주문 확인</button>
+      </form>
+    </div>
+  );
+};
+
+export default SummaryPage;
