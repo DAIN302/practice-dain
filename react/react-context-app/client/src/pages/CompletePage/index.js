275ae81058505a0822921f46f9f19bb06085ef7f
DAIN302 <dbgnfk12@gmail.com>
2024-10-12 22:55:50 +0900

    주문 확인 페이지, 주문 완료 페이지

--- react/react-context-app/client/src/pages/CompletePage/index.js
+++ react/react-context-app/client/src/pages/CompletePage/index.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { OrderContext } from "../../context/OrderContext";
 
-const CompletePage = () => {
-  return (
-    <div>CompletePage</div>
-  )
-}
+// 주문 완료 페이지
+const CompletePage = ({ setStep }) => {
+  const [orderData] = useContext(OrderContext);
+  const [orderHistory, setOrderHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default CompletePage
+  useEffect(() => {
+    orderCompleted(orderData);
+  }, [orderData]);
+
+  const orderCompleted = async (orderData) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:4000/order`,
+        orderData
+      );
+
+      setOrderHistory(response.data);
+      setLoading(false);
+    } catch (error) {}
+  };
+
+  const orderTable = orderHistory.map((item) => (
+    <tr key={item.orderNumber}>
+      <td>{item.orderNumber}</td>
+      <td>{item.price}</td>
+    </tr>
+  ));
+
+  if (loading) {
+    return <div>...loading</div>;
+  } else {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h2>주문이 성공했습니다.</h2>
+        <h3>지금까지 모든 주문</h3>
+        <table style={{ margin: "auto" }}>
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderTable}
+          </tbody>
+        </table>
+        <br />
+        <button onClick={() => setStep(0)}>첫 페이지로</button>
+      </div>
+    );
+  }
+};
+
+export default CompletePage;
--- react/react-context-app/client/src/pages/SummaryPage/index.js
+++ react/react-context-app/client/src/pages/SummaryPage/index.js
@@ -1,13 +1,46 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { OrderContext } from "../../context/OrderContext";
 
-const SummaryPage = () => {
+const SummaryPage = ({ setStep }) => {
   // 주문 확인 체크를 해야 주문 확인 버튼 누를 수 있음
-
   const [checked, setChecked] = useState(false);
+
+  const [orderDetails] = useContext(OrderContext);
+
+  // Array.from()
+  // 유사한 배열 객체나 반복 가능한 객체를 얕게 복사해서 새로운 array 객체를 만드는 메서드
+  const productArray = Array.from(orderDetails.products);
+  const productList = productArray.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ));
+
+  const hasOptions = orderDetails.options.size > 0;
+  let optionsDisplay = null;
+  if (hasOptions) {
+    const optionsArray = Array.from(orderDetails.options);
+    const optionList = optionsArray.map((key) => <li key={key}>{key}</li>);
+    optionsDisplay = (
+      <>
+        <h2>옵션 : {orderDetails.totals.options}</h2>
+        <ul>{optionList}</ul>
+      </>
+    );
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStep(2);
+  };
 
   return (
     <div>
-      <form>
+      <h1>주문 확인</h1>
+      <h2>여행 상품 : {orderDetails.totals.products}</h2>
+      <ul>{productList}</ul>
+      {optionsDisplay}
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           checked={checked}
@@ -16,7 +49,9 @@ const SummaryPage = () => {
         />{" "}
         <label htmlFor="confirm-checkbox">주문하려는 것을 확인하셨나요?</label>
         <br />
-        <button type="submit" disabled={!checked}>주문 확인</button>
+        <button type="submit" disabled={!checked}>
+          주문 확인
+        </button>
       </form>
     </div>
   );
