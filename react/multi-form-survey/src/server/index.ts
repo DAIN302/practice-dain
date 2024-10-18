0f7b69b3acf29cd278847b384693f72d1add9e0c
DAIN302 <dbgnfk12@gmail.com>
2024-10-18 16:05:56 +0900

    feat : express 서버 설정

---
+++ react/multi-form-survey/src/server/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import {createServer as createViteServer} from 'vite';
+
+async function startServer() {
+    const app = express();
+    const port = 5173;
+
+    // 미들웨어 등록
+    app.use(express.json());
+
+    // 서버 생성
+    const vite = await createViteServer({
+        server : {middlewareMode: true},
+        appType : 'spa'
+    })
+
+    // 서버 미들웨어 형태로 등록
+    app.use(vite.middlewares);
+
+    return app.listen(port);
+}
+
+const server = await startServer();
+
+// vite dev 서버와 충돌 방지를 위한 설정
+if(import.meta.hot){
+    import.meta.hot.on('vite:beforeFullReload', () => {
+        server.close();
+    })
+
+    import.meta.hot.dispose(() => {
+        server.close();
+    })
+}
