5c79da9b03769cf717c135d5db1ab15103deab48
DAIN302 <dbgnfk12@gmail.com>
2024-11-30 23:01:24 +0900

    study : 도시 상세 모달 추가

--- react/travel-plan/client/src/components/home/CityList.tsx
+++ react/travel-plan/client/src/components/home/CityList.tsx
@@ -1,20 +1,43 @@
 import { City } from "@/types";
 import Card from "./Card";
+import { useModalStore } from "@/store";
+import Modal, { ModalBackdrop, ModalPanel } from "../common/Modal";
+import CloseIcon from "@/assets/icons/close.svg?react";
+import CityDetail from "./CityDetail";
 
 interface Props {
   cities: City[];
 }
 
 export default function CityList({ cities }: Props) {
+  const { openModal } = useModalStore();
+
+  const openDetailModal = (city: City) => {
+    openModal(({ onClose }) => (
+      <Modal>
+        <ModalBackdrop />
+        <ModalPanel>
+          <div className="rounded-20 border border-gray100 bg-white px-28 pt-58 pb-37 relative w-[655px] min-h-[300px]">
+            <button className="absolute right-28 top-22" onClick={onClose}>
+              <CloseIcon />
+            </button>
+            <CityDetail city={city} />
+          </div>
+        </ModalPanel>
+      </Modal>
+    ));
+  };
+
   return (
     <div className="flex flex-wrap justify-between gap-y-28 items-start w-full">
       {cities.map((city) => (
-        <Card
-          key={city.code}
-          title={city.nameEn.toUpperCase()}
-          description={`${city.country.name} ${city.name}`}
-          image={city.thumbnail}
-        />
+        <button key={city.code} onClick={() => openDetailModal(city)}>
+          <Card
+            title={city.nameEn.toUpperCase()}
+            description={`${city.country.name} ${city.name}`}
+            image={city.thumbnail}
+          />
+        </button>
       ))}
     </div>
   );
