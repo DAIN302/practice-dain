cb5a4f4b3a4eb8efd0bf48ab09bcbc3351fb6e76
DAIN302 <dbgnfk12@gmail.com>
2025-03-24 17:54:46 +0900

    initial commit

---
+++ react/music-app/packages/music-server/esbuild.js
@@ -0,0 +1,22 @@
+// 서버 실행 번들러 설정
+const esbuild = require("esbuild");
+
+const config = {
+  entryPoints: ["./src/index.ts"],
+  bundle: true,
+  platform: "node",
+  outfile: "./dist/server.js",
+  target: "node14",
+  sourcemap: true,
+};
+
+const run = async () => {
+  if (process.argv.includes("--watch")) {
+    const ctx = await esbuild.context(config);
+    await ctx.watch();
+  } else {
+    await esbuild.build(config);
+  }
+};
+
+run();
