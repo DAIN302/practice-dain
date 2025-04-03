4c665a3031489f0213d25cdafc4bc9610563df67
DAIN302 <dbgnfk12@gmail.com>
2025-04-03 19:31:31 +0900

    study : next font google

--- next-study/src/domains/next-functions/font/google/index.tsx
+++ next-study/src/domains/next-functions/font/google/index.tsx
@@ -1,7 +1,28 @@
+import { noto_sans } from "@/styles/fonts";
 import { FC } from "react";
+import styles from './index.module.css';
+import Head from "next/head";
 
 const GoogleMain:FC = () => {
-    return <main>Google</main>
+    return (
+      <main>
+        <p style={{fontSize:50}}>Font : Default</p>
+        <p>CLS TEST</p>
+        <hr />
+        <p className={noto_sans.className} style={{fontSize:50}}>Font : Noto Sans</p>
+        <p>CLS TEST</p>
+        <hr />
+        {/* <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&display=swap" rel="stylesheet">
+        </Head> */}
+        
+        {/* <p className={styles.baskervville} style={{fontSize:50}}>Font : Remote</p> */}
+        {/* <p>CLS TEST</p> */}
+
+      </main>
+)
 }
 
 export { GoogleMain }
--- next-study/src/styles/fonts/index.ts
+++ next-study/src/styles/fonts/index.ts
@@ -1,5 +1,7 @@
 import nextLocalFont from 'next/font/local';
+import { Noto_Sans } from 'next/font/google';
 
+// 로컬 폰트
 const gilroy = nextLocalFont({
     src:[
         {
@@ -18,8 +20,19 @@ const gilroy = nextLocalFont({
     fallback: ["system-ui"]
 })
 
-export { gilroy };
+// 구글 폰트
+const noto_sans = Noto_Sans({
+    subsets: ['latin'],
+    weight: ['400', '600', '800'],
+    display: "swap",
+    fallback: ['system-ui'],
+    preload: true, // 폰트 빠르게 로딩
+    adjustFontFallback: true // CLS 방지
+})
+
+export { gilroy, noto_sans };
 
 // export 로 한 이유
 // 폰트를 하나의 인스턴스로 생성했기 때문에 호출할 때마다 폰트 인스턴스가 생성됨 (불필요한 메모리)
 // 따라서 export 해서 인스턴스가 하나만 생성되도록 함
+// next 에서 권장하는 방법!
